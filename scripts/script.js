a5d848fe7d251be4701dc58d7c197b5a40977b0a
MindfulLearner <dimajosh1009@gmail.com>
2024-08-29 16:18:54 +0200

    swagg love of coding

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,16 @@
 // script
 
+const { createApp } = Vue; 
 
-
+createApp (
+    {
+        data() { 
+            return {
+                nome: 'josh',
+            }
+        }
+    }
+).mount('#app');
 
 
 
